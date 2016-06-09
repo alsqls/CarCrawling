c79e610f6dc3f60755c587bae3bce320d8bcbfc3
alsqls <tpgml9247@gmail.com>
2016-06-10 02:55:55 +0900

    ddddddddd

--- carCrawling.py
+++ carCrawling.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 def Crawling():
     titles = []
+    fuel_num = []
     for page in range(1,15):
         html = Request('http://auto.danawa.com/newcar/?Work=search&Tab=&Brand=&Classify=&Fuel=&Tm=&Price=&Efficiency=&EfficiencyKind=&DriveWheel=&Displace=&ListType=list&Order=2&Punit=20&ListType=list&Page=' + str(page) , headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
         webpage = urlopen(html).read()
@@ -13,14 +14,13 @@ def Crawling():
         for i in car_title:
             titles.append(i.text)
         titles = [re.replace("\n","") for re in titles]
-        print(titles)
-
+        #print(titles)
 
         car_fuel = soup.find_all("div" , class_="spec info")
         print(len(car_fuel))
-
         for f in car_fuel :
-            fuel_num = f.find_all("span", text = '㎞/ℓ')
+            fuel_num.append(f.find_all("span", text = '㎞/ℓ').getText())
+        fuel_num = [re.replace("\n","") for re in fuel_num]
         print(fuel_num)
         car_price = soup.find_all("div" ,class_="price")
 
