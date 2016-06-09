7146f840a0ad9e14a1bb6e909ac9a80b9cf0e629
alsqls <tpgml9247@gmail.com>
2016-06-10 02:44:21 +0900

    title

--- carCrawling.py
+++ carCrawling.py
@@ -3,18 +3,23 @@ import urllib.parse
 from bs4 import BeautifulSoup
 
 def Crawling():
-    pages = 10;
+    titles = []
     for page in range(1,15):
         html = Request('http://auto.danawa.com/newcar/?Work=search&Tab=&Brand=&Classify=&Fuel=&Tm=&Price=&Efficiency=&EfficiencyKind=&DriveWheel=&Displace=&ListType=list&Order=2&Punit=20&ListType=list&Page=' + str(page) , headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage , from_encoding="utf-8")
-        #car_title = soup.find_all("div" ,class_="title") # 배열
+
+        car_title = soup.find_all("div" ,class_="title") # 배열
+        for i in range(len(car_title)):
+            titles.append(i.text)
+        print(titles)
+
         car_fuel = soup.find_all("div" , class_="spec info")
         print(len(car_fuel))
-        #for f in car_fuel :
-            #fuel_num = f.find_all("span", text = '㎞/ℓ')
-        #print(fuel_num)
-        #print(car_fuel)
+
+        for f in car_fuel :
+            fuel_num = f.find_all("span", text = '㎞/ℓ')
+        print(fuel_num)
         car_price = soup.find_all("div" ,class_="price")
 
 # text = '키워드'
