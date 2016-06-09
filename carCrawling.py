69962e00c90373a4b74be051cdd35291c591e3ca
alsqls <tpgml9247@gmail.com>
2016-06-10 00:28:26 +0900

    ddddd

--- carCrawling.py
+++ carCrawling.py
@@ -8,10 +8,14 @@ def Crawling():
         html = Request('http://auto.danawa.com/newcar/?Work=search&Tab=&Brand=&Classify=&Fuel=&Tm=&Price=&Efficiency=&EfficiencyKind=&DriveWheel=&Displace=&ListType=list&Order=2&Punit=20&ListType=list&Page=' + str(page) , headers={'User-Agent':'Mozilla/6.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage , from_encoding="utf-8")
-        carNum = soup.find_all("div" ,class_="num")
-        print(len(carNum))
         car_title = soup.find_all("div" ,class_="title") # 배열
         print(car_title)
+
+        car_fuel = soup.find("div" , { "class" : "spec info"} ).next_element.next_element.next_element.find_all("span",class_="num")
+        print(car_fuel)
+        car_price = soup.find_all("div" ,class_="price")
+        page = page+1
+
 
 
 
