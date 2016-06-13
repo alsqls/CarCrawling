86b1aa6380fe4c5c0486cbb823e45dce98b28352
alsqls <tpgml9247@gmail.com>
2016-06-14 02:42:18 +0900

    dddd
    ddd

--- carCrawling.py
+++ carCrawling.py
@@ -1,17 +1,17 @@
 from urllib.request import Request, urlopen
 import urllib.parse
 from bs4 import BeautifulSoup
-
+#-*- coding:utf-8 -*-
 def Crawling():
-    titles = []
-    fuel_num = []
-    price_num = []
+    titles = [] # 차 이름
+    fuel_num = [] # 차 연비
+    price_num = [] # 차 가격
     for page in range(1,15):
         html = Request('http://auto.danawa.com/newcar/?Work=search&Tab=&Brand=&Classify=&Fuel=&Tm=&Price=&Efficiency=&EfficiencyKind=&DriveWheel=&Displace=&ListType=list&Order=2&Punit=20&ListType=list&Page=' + str(page) , headers={'User-Agent':'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'})
         webpage = urlopen(html).read()
         soup = BeautifulSoup(webpage , from_encoding="utf-8")
 
-        car_title = soup.find_all("div" ,class_="title") # 배열
+        car_title = soup.find_all("div" ,class_="title")
         for i in car_title:
             titles.append(i.text)
         titles = [re.replace("\n","") for re in titles]
@@ -20,7 +20,7 @@ def Crawling():
         car_fuel = soup.find_all("div" , class_="spec info")
         print(len(car_fuel)) #20
         for f in car_fuel :
-            fuel_num.append(f.find_all("span", class_="num"))
+            fuel_num.append(f.find_all("span", class_="num").text.strip())
         print(fuel_num)
 
         car_price = soup.find_all("div" ,class_="price")
